9273805c939dbfcd9835ae2a9bf815cb340f09b2
Christian Lysaker <chr_414@hotmail.com>
2014-03-05 13:21:37 +0100

    Fixed require-bug test in RobotController

--- Main/src/com/eit/control/RobotController.java
+++ Main/src/com/eit/control/RobotController.java
@@ -58,7 +58,7 @@ public class RobotController implements ImageProcessListener {
 
             if (requires == StateRequirement.BALL) {
                 eyeProcessing.startBallDetection();
-            } else if (requires == StateRequirement.BALL) {
+            } else if (requires == StateRequirement.COLLECTION_BOX) {
                 eyeProcessing.startBoxDetection();
             } else {
                 stateManager.step();
