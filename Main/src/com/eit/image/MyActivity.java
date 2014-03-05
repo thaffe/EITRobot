0c7391bff090a1b02564e0f6c1529b5bb599e09a
Fredrik Stenbro <fredrik.stenbro@hotmail.com>
2014-03-05 12:39:01 +0100

    div

--- Main/src/com/eit/image/MyActivity.java
+++ Main/src/com/eit/image/MyActivity.java
@@ -1,18 +1,63 @@
 package com.eit.image;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.eit.bluetooth.BluetoothCommunicator;
+import com.eit.bluetooth.NXTTalker;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.core.Mat;
 
 import java.util.ArrayList;
 
+
+
+
 public class MyActivity extends Activity implements ImageProcessListener{
     ImageProcessing imgP;
+
+    private boolean NO_BT = false;
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_CONNECT_DEVICE = 2;
+    private static final int REQUEST_SETTINGS = 3;
+
+    public static final int MESSAGE_TOAST = 1;
+    public static final int MESSAGE_STATE_CHANGE = 2;
+
+    public static final String TOAST = "toast";
+
+    private static final int MODE_BUTTONS = 1;
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
+    private BluetoothCommunicator btCommunicator;
+
+    private int mState = NXTTalker.STATE_NONE;
+    private int mSavedState = NXTTalker.STATE_NONE;
+    private boolean mNewLaunch = true;
+    private String mDeviceAddress = null;
+    private TextView mStateDisplay;
+    private Button mConnectButton;
+    private Button mDisconnectButton;
+
+    private int mPower = 80;
+    private int mControlsMode = MODE_BUTTONS;
+
+    private boolean mReverse;
+    private boolean mReverseLR;
+    private boolean mRegulateSpeed;
+    private boolean mSynchronizeMotors;
 
     public MyActivity() {
         imgP = new ImageProcessing(this, this);
@@ -22,6 +67,21 @@ public class MyActivity extends Activity implements ImageProcessListener{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "NXT Remote Control");
+
+        if (!NO_BT) {
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            if (mBluetoothAdapter == null) {
+                Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+        }
+
+
         setContentView(R.layout.main);
         imgP.create();
     }
