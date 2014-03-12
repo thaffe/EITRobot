481c63622f371aedabc17d9ec22f271b516e1872
= <=>
2014-03-12 09:45:49 +0100

    Ting

--- Main/src/com/eit/image/MyActivity.java
+++ Main/src/com/eit/image/MyActivity.java
@@ -16,6 +16,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.eit.R;
 import com.eit.bluetooth.BluetoothCommunicator;
 import com.eit.bluetooth.ChooseDeviceActivity;
 import com.eit.bluetooth.NXTTalker;
@@ -65,226 +66,24 @@ public class MyActivity extends Activity implements ImageProcessListener{
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "NXT Remote Control");
-
-        if (!NO_BT) {
-            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-            if (mBluetoothAdapter == null) {
-                Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
-                finish();
-                return;
-            }
-        }
-
-        setupUI();
-
-        NXTTalker mNXTTalker = new NXTTalker(mHandler);
-        btCommunicator = new BluetoothCommunicator(mNXTTalker);
-
-        //setContentView(R.layout.main);
-        imgP.create();
-    }
-
-    public void setupUI() {
-        //mStateDisplay = (TextView) findViewById(R.id.state_display);
-
-        //mConnectButton = (Button) findViewById(R.id.connect_button);
-        mConnectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!NO_BT) {
-                    findBrick();
-                } else {
-                    mState = NXTTalker.STATE_CONNECTED;
-                    displayState();
-                }
-            }
-        });
-
-       // mDisconnectButton = (Button) findViewById(R.id.disconnect_button);
-        mDisconnectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //mNXTTalker.stop();
-            }
-        });
-
-        displayState();
-    }
-
-    private void displayState() {
-        String stateText = null;
-        int color = 0;
-        switch (mState){
-            case NXTTalker.STATE_NONE:
-                stateText = "Not connected";
-                color = 0xffff0000;
-                mConnectButton.setVisibility(View.VISIBLE);
-                mDisconnectButton.setVisibility(View.GONE);
-                setProgressBarIndeterminateVisibility(false);
-                if (mWakeLock.isHeld()) {
-                    mWakeLock.release();
-                }
-                break;
-            case NXTTalker.STATE_CONNECTING:
-                stateText = "Connecting...";
-                color = 0xffffff00;
-                mConnectButton.setVisibility(View.GONE);
-                mDisconnectButton.setVisibility(View.GONE);
-                setProgressBarIndeterminateVisibility(true);
-                if (!mWakeLock.isHeld()) {
-                    mWakeLock.acquire();
-                }
-                break;
-            case NXTTalker.STATE_CONNECTED:
-                stateText = "Connected";
-                color = 0xff00ff00;
-                mConnectButton.setVisibility(View.GONE);
-                mDisconnectButton.setVisibility(View.VISIBLE);
-                setProgressBarIndeterminateVisibility(false);
-                if (!mWakeLock.isHeld()) {
-                    mWakeLock.acquire();
-                }
-                break;
-        }
-        mStateDisplay.setText(stateText);
-        mStateDisplay.setTextColor(color);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //Log.i("NXT", "NXTRemoteControl.onStart()");
-        if (!NO_BT) {
-            if (!mBluetoothAdapter.isEnabled()) {
-                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-            } else {
-                if (mSavedState == NXTTalker.STATE_CONNECTED) {
-                    BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(mDeviceAddress);
-                    //mNXTTalker.connect(device);
-                    btCommunicator.connect(device);
-                } else {
-                    if (mNewLaunch) {
-                        mNewLaunch = false;
-                        findBrick();
-                    }
-                }
-            }
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (mState == NXTTalker.STATE_CONNECTED) {
-            outState.putString("device_address", mDeviceAddress);
-        }
-        //outState.putBoolean("reverse", mReverse);
-        outState.putInt("power", mPower);
-        outState.putInt("controls_mode", mControlsMode);
-    }
-
-    private final Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MESSAGE_TOAST:
-                    Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST), Toast.LENGTH_SHORT).show();
-                    break;
-                case MESSAGE_STATE_CHANGE:
-                    mState = msg.arg1;
-                    displayState();
-                    break;
-            }
-        }
-    };
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        //Log.i("NXT", "NXTRemoteControl.onStop()");
-        mSavedState = mState;
-        // mNXTTalker.stop();
-        if (mWakeLock.isHeld()) {
-            mWakeLock.release();
-        }
-    }
-
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        readPreferences(sharedPreferences, key);
-    }
-
-    private void readPreferences(SharedPreferences prefs, String key) {
-        if (key == null) {
-            mReverse = prefs.getBoolean("PREF_SWAP_FWDREV", false);
-            mReverseLR = prefs.getBoolean("PREF_SWAP_LEFTRIGHT", false);
-            mRegulateSpeed = prefs.getBoolean("PREF_REG_SPEED", false);
-            mSynchronizeMotors = prefs.getBoolean("PREF_REG_SYNC", false);
-            if (!mRegulateSpeed) {
-                mSynchronizeMotors = false;
-            }
-        } else if (key.equals("PREF_SWAP_FWDREV")) {
-            mReverse = prefs.getBoolean("PREF_SWAP_FWDREV", false);
-        } else if (key.equals("PREF_SWAP_LEFTRIGHT")) {
-            mReverseLR = prefs.getBoolean("PREF_SWAP_LEFTRIGHT", false);
-        } else if (key.equals("PREF_REG_SPEED")) {
-            mRegulateSpeed = prefs.getBoolean("PREF_REG_SPEED", false);
-            if (!mRegulateSpeed) {
-                mSynchronizeMotors = false;
-            }
-        } else if (key.equals("PREF_REG_SYNC")) {
-            mSynchronizeMotors = prefs.getBoolean("PREF_REG_SYNC", false);
-        }
-    }
-
-    private void findBrick() {
-        Intent intent = new Intent(this, ChooseDeviceActivity.class);
-        startActivityForResult(intent, REQUEST_CONNECT_DEVICE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_ENABLE_BT:
-                if (resultCode == Activity.RESULT_OK) {
-                    findBrick();
-                } else {
-                    Toast.makeText(this, "Bluetooth not enabled, exiting.", Toast.LENGTH_LONG).show();
-                    finish();
-                }
-                break;
-            case REQUEST_CONNECT_DEVICE:
-                if (resultCode == Activity.RESULT_OK) {
-                    String address = data.getExtras().getString(ChooseDeviceActivity.EXTRA_DEVICE_ADDRESS);
-                    BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
-                    //Toast.makeText(this, address, Toast.LENGTH_LONG).show();
-                    mDeviceAddress = address;
-                    btCommunicator.connect(device);
-                    //mNXTTalker.connect(device);
-                }
-                break;
-            case REQUEST_SETTINGS:
-                //XXX?
-                break;
-        }
-    }
-
-    @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
         imgP.resume();
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.main);
+        imgP.create();
+    }
+
+    @Override
     public void OnBallDetect(ArrayList<Ball> balls) {
-        Log.i(ImageProcessing.TAG, "FOUND SOME BALLS:" + balls.size());
+        for (Ball ball : balls) {
+            Log.i(ImageProcessing.TAG,String.format("Hoffset:%f    Distance:%f",ball.getHorizontalOffset(), ball.getDistance()));
+        }
     }
 
     @Override
